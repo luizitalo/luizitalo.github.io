3c3609a278243b4129e5a1b7ee5a224ef2d192aa
Luiz Italo <luiz_italonunes@hotmail.com>
2023-04-04 16:54:17 -0300

    primeira versao

---
+++ anima.js
@@ -0,0 +1,37 @@
+const numeros = document.querySelectorAll('.qwer');
+numeros.forEach((numero) => {
+    const total = numero.innerText;
+
+    let start = 0;
+    const timer = setInterval(() =>{
+        start++; 
+       numero.innerText = start
+        if (start >= total)
+        clearInterval(timer)
+    }, 120)
+});
+
+
+var paragrafo = document.querySelector('.pp')
+var text =  paragrafo.innerHTML
+var index = 0
+
+
+const escrever = () =>{
+    paragrafo.innerHTML = paragrafo.innerHTML.replace('', '')
+
+    if (text.length > index){
+        if (index == 0){
+            paragrafo.innerHTML = text.charAt(index)
+        }else {
+            paragrafo.innerHTML += text.charAt(index)
+        }
+        paragrafo.innerHTML += ''
+
+        index++
+        setTimeout(escrever, 100)
+    }
+}
+
+escrever()
+
